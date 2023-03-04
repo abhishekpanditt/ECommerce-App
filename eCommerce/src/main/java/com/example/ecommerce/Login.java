be25b8c6017015da882f7ebe71ca6e05674cdec5
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-03-04 15:52:23 +0530

    Update Login.java

--- eCommerce/src/main/java/com/example/ecommerce/Login.java
+++ eCommerce/src/main/java/com/example/ecommerce/Login.java
@@ -9,8 +9,8 @@ public class Login {
 
     private static byte[] getSha(String input){
         try{
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            return md.digest(input.getBytes(StandardCharsets.UTF_8));
+            MessageDigest md = MessageDigest.getInstance("SHA-256");    //this class supports algorithms
+            return md.digest(input.getBytes(StandardCharsets.UTF_8));           //gives fixed length hashing value
         }
         catch(Exception e){
             e.printStackTrace();
@@ -18,7 +18,7 @@ public class Login {
         return null;
     }
 
-    private static String getEncryptedPassword(String password){
+    private static String getEncryptedPassword(String password){                        //password encryption
         try{
             BigInteger num = new BigInteger(1, getSha(password));
             StringBuilder hexString = new StringBuilder(num.toString(16));
@@ -30,7 +30,7 @@ public class Login {
         return null;
     }
 
-    public static Customer customerLogin(String userEmail, String password){
+    public static Customer customerLogin(String userEmail, String password){                    //matching customer data with database
         //SELECT * FROM customer WHERE email = 'rohan@gmail.com' AND password = 'abc';
         String encryptedPass = getEncryptedPassword(password);
         String query = "SELECT * FROM customer WHERE email = '"+ userEmail +"' AND password = '"+ encryptedPass +"'";
